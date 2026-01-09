ce2058d3e7ec2e5bbd0350a581c0017130e88c60
mubashirnanath <mubashirnanath@gmail.com>
2026-01-09 10:41:50 +0530

    problem 53 completed

--- blind75/53-maximum-subarray.py
+++ blind75/53-maximum-subarray.py
@@ -3,10 +3,10 @@ def maxSubArray(nums):
     currSum = 0
 
     for num in nums:
-        if currSum<0:
+        if currSum<0:   # if currSum become -ve then make it 0, because we dont get higher number when we add anything with -ve number
             currSum = 0
-        currSum = currSum+num
-        maxSum = max(maxSum, currSum)
+        currSum = currSum+num        # add current number to currSum
+        maxSum = max(maxSum, currSum) # check which is largest
     return maxSum
 
 
